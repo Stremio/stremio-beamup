80f82761a0467c40947b8157129fdb4ecce33881
Ivo Georgiev <ivo@strem.io>
2020-04-02 22:19:26 +0300

    main.tf: use ubuntu, deploy key, install dokku

--- main.tf
+++ main.tf
@@ -1,24 +1,48 @@
 provider "cherryservers" {
   auth_token = "${trimspace(file("./creds/cherryservers"))}"
 }
+variable "private_key" {
+  default = "./id_deploy"
+}
 variable "region" {
   default = "EU-East-1"
 }
 variable "image" {
-  default = "Debian 10 64bit"
+  default = "Ubuntu 18.04 64bit"
 }
+
+# corresponds to ssd_smart16
 variable "plan_id" {
-  #default = "ssd_smart16"
   default = "94"
 }
 
+resource "cherryservers_ssh" "tf_deploy_key" {
+  name   = "tf_deploy_key"
+  public_key = "${file("${var.private_key}.pub")}"
+}
+
+
 # The controller/deployer server
-resource "cherryservers_server" "main-server" {
+resource "cherryservers_server" "deployer" {
   project_id = "101781"
   region = "${var.region}"
   hostname = "stremio-addon-deployer"
   image = "${var.image}"
   plan_id = "${var.plan_id}"
-  #ssh_keys_ids = [
-  #  "${cherryservers_ssh.tf_deploy_key.id}"]
+  ssh_keys_ids = ["${cherryservers_ssh.tf_deploy_key.id}"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget https://raw.githubusercontent.com/dokku/dokku/v0.20.0/bootstrap.sh",
+      "DOKKU_TAG=v0.20.0 bash bootstrap.sh"
+    ]
+
+    connection {
+      type = "ssh"
+      user = "root"
+      host = "${cherryservers_server.deployer.primary_ip}"
+      private_key = "${file(var.private_key)}"
+      timeout = "30m"
+    }
+  }
 }
