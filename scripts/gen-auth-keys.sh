5ac7b959c207591611b48e06b92114b34a9df6c5
Ivo Georgiev <ivo@strem.io>
2020-04-03 00:12:01 +0300

    beamup-sync-keys almost done, beamup-entry cleaned up

--- scripts/gen-auth-keys.sh
+++ scripts/gen-auth-keys.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 GITHUB_USER=Ivshti
-
-curl --silent "https://github.com/$GITHUB_USER.keys" | while read key
+set -e
+KEYS=`curl --silent "https://github.com/$GITHUB_USER.keys"`
+( echo $KEYS | while read SSH_KEY
 do
 	# environment= does not seem to be working...
 	# "restrict" instead of "no-agent-forwarding,no-port-forwarding,no-x11-forwarding": this also disables the pty
 	# the PTY is not needed cause we don't need anything interactive
-	echo command=\"GITHUB_USER=\'$GITHUB_USER\' /tmp/script2.sh $SSH_ORIGINAL_COMMAND\",restrict $key
-done
-
-echo command=\"/tmp/script2.sh\",restrict `cat ./id_control_ed25519.pub`
+	echo "command=\"GITHUB_USER='$GITHUB_USER' /tmp/script2.sh \$SSH_ORIGINAL_COMMAND\",restrict $SSH_KEY"
+done ) && echo command=\"/tmp/script2.sh\",restrict `cat ./id_control_ed25519.pub`
