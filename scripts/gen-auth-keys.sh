6733d2798abf3980f7d2974f4b986d5a8f4d9854
Ivo Georgiev <ivo@strem.io>
2020-04-02 22:09:25 +0300

    pass SSH_ORIGINAL_COMMAND to the entrypoint

--- scripts/gen-auth-keys.sh
+++ scripts/gen-auth-keys.sh
@@ -5,7 +5,8 @@ curl --silent "https://github.com/$GITHUB_USER.keys" | while read key
 do
 	# environment= does not seem to be working...
 	# "restrict" instead of "no-agent-forwarding,no-port-forwarding,no-x11-forwarding": this also disables the pty
-	echo command=\"GITHUB_USER=\'$GITHUB_USER\' /tmp/script2.sh\",restrict $key
+	# the PTY is not needed cause we don't need anything interactive
+	echo command=\"GITHUB_USER=\'$GITHUB_USER\' /tmp/script2.sh $SSH_ORIGINAL_COMMAND\",restrict $key
 done
 
 echo command=\"/tmp/script2.sh\",restrict `cat ./id_control_ed25519.pub`
