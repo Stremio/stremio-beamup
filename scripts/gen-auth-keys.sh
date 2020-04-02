a7435f42a594d9a750905d5e49eec5b182b80544
Ivo Georgiev <ivo@strem.io>
2020-04-02 16:23:26 +0300

    gen-auth-keys: PTY is not needed, add control key

--- scripts/gen-auth-keys.sh
+++ scripts/gen-auth-keys.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 GITHUB_USER=Ivshti
 
-curl --silent https://github.com/$GITHUB_USER.keys | while read key
+curl --silent "https://github.com/$GITHUB_USER.keys" | while read key
 do
 	# environment= does not seem to be working...
-	echo command=\"/tmp/script2.sh $GITHUB_USER\",restrict,pty $key
+	# "restrict" instead of "no-agent-forwarding,no-port-forwarding,no-x11-forwarding": this also disables the pty
+	echo command=\"GITHUB_USER=\'$GITHUB_USER\' /tmp/script2.sh\",restrict $key
 done
+
+echo command=\"/tmp/script2.sh\",restrict `cat ./id_control_ed25519.pub`
