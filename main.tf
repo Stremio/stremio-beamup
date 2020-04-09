397e81df8b15ad45e3126fae6daf9afe7fc2603f
Ivo Georgiev <ivo@strem.io>
2020-04-10 00:08:14 +0300

    beamup-sync-swarm generates an nginx config too

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "cherryservers_server" "swarm" {
     inline = [
 		"export CHANNEL=stable",
 		"wget -nv -O - https://get.docker.com/ | sh",
-		"apt install -y vim nodejs",
+		"apt install -y vim nodejs nginx",
 		"echo -e 'net.ipv6.conf.all.disable_ipv6=1\nnet.ipv6.conf.default.disable_ipv6=1\nnet.ipv6.conf.lo.disable_ipv6=1' >>/etc/sysctl.conf",
 		"sysctl -p",
 	]
